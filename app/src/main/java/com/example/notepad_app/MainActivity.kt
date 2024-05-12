f9b89a28aa8ee86e23b512b7278402409e9443a5
DulinaSH2001 <dulinahejithaindrawansha@gmail.com>
2024-05-12 09:25:11 +0530

    add intent to addbtn

--- app/src/main/java/com/example/notepad_app/MainActivity.kt
+++ app/src/main/java/com/example/notepad_app/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.example.notepad_app
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.notepad_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.Addbtn.setOnClickListener {
+            val intent = Intent (this,Add_note::class.java)
+            startActivity(intent)
         }
+
+
     }
 }
---
+++ app/src/main/java/com/example/notepad_app/note.kt
@@ -0,0 +1,3 @@
+package com.example.notepad_app
+
+data class note()
